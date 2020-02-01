7834d9d98c873662c54fb86e301c5a974e06f515
timxor <tcsiwula@gmail.com>
2020-01-31 20:36:31 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

---
+++ HelloWorld.scala
@@ -0,0 +1,32 @@
+/*
+--------------------------------------------------------
+file: HelloWorld.scala
+date: fri jan 31 2020
+author: tim siwula
+--------------------------------------------------------
+path: /Users/tim.siwula/Desktop/projects/scala
+--------------------------------------------------------
+compile: scalac HelloWorld.scala
+--------------------------------------------------------
+execute: scala HelloWorld
+--------------------------------------------------------
+*/
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    //this is my first Scala program
+    println("Hello, world!")
+    /*
+     * declare variables
+     */
+    var firstName:String = "Peggy"
+    val age:Int = 21
+    println(firstName + ", is \n"+age+" years old")
+    var sum = 10 +
+    25
+    def function1:Unit = {
+      println("This is function1")
+    }
+    function1
+  }
+}
